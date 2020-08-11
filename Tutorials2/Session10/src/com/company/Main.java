090383cec71355e58f4d6cc0cc2052b1ca642a9c
Nihan Abaci <nihanabaci@Nihans-MacBook-Air.local>
2020-08-11 17:12:45 -0500

    Deleting unnesesary code

--- Tutorials2/Session10/src/com/company/Main.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Main() {
-    char[] charArray = {'H', 'e', 'l', 'l', 'o'};
-    String charString = new String(charArray);
-    System.out.println(charString);
-}
-
